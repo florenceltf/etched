c20892993a0af98c272a8e38f9beb75ab9c83424
Florence Li <florenceli@Florences-MBP.gateway>
2020-10-05 23:22:37 +1100

    arrow

--- static/js/mobile-menu.js
+++ static/js/mobile-menu.js
@@ -3,16 +3,12 @@ const menuBackground = document.querySelector(".menu-background");
 const menuText = document.querySelector(".menu-text");
 const homeOverlay = document.querySelector(".home");
 const scrollTag = document.querySelector(".scroll");
-const containerTag = document.querySelector(".container");
-
 
 menuTag.addEventListener("click", function () {
   if (menuText.innerHTML === "Close") {
     menuBackground.style.display = "none";
     menuText.innerHTML = "Menu";
     homeOverlay.style.display = "flex";
-    containerTag.style.display = "block";
-
   } else {
     menuBackground.style.display = "flex";
     homeOverlay.style.display = "none";
