bb60ff99c0ad9ca8de416a570b543afe6d628636
Florence Li <florenceli@Florences-MBP.gateway>
2020-10-07 07:22:52 +1100

    mobile menu underline

--- static/js/mobile-menu.js
+++ static/js/mobile-menu.js
@@ -1,11 +1,13 @@
 const menuTag = document.querySelector(".mobile-menu");
 const menuBackground = document.querySelector(".menu-background");
+console.log("menuBackground", menuBackground)
 const menuText = document.querySelector(".menu-text");
 const homeOverlay = document.querySelector(".home");
 const scrollTag = document.querySelector(".scroll");
 const topTag = document.querySelector("hr.top");
 const middleTag = document.querySelector("hr.middle");
 const bottomTag = document.querySelector("hr.bottom");
+const logoTag = document.querySelector(".logo-container");
 
 topTag.classList.remove("top");
 middleTag.classList.remove("middle");
@@ -13,6 +15,7 @@ bottomTag.classList.remove("bottom");
 
 
 menuTag.addEventListener("click", function () {
+  console.log("hi")
   if (menuText.innerHTML === "Close") {
     menuBackground.classList.remove("fadein");
     menuBackground.classList.add("fadeout");
@@ -21,6 +24,7 @@ menuTag.addEventListener("click", function () {
     topTag.classList.remove("top");
     middleTag.classList.remove("middle");
     bottomTag.classList.remove("bottom");
+    logoTag.style.display = "block";
   } else {
     menuBackground.style.display = "flex";
     homeOverlay.style.display = "none";
@@ -29,6 +33,7 @@ menuTag.addEventListener("click", function () {
     topTag.classList.add("top");
     middleTag.classList.add("middle");
     bottomTag.classList.add("bottom");
+    logoTag.style.display = "none";
   }
 });
 
